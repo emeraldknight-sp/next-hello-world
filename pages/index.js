7e968708355f1823fc036db7ec3aac47edf6df93
David Almeida <contato.davidalmeida@outlook.com>
2023-01-01 20:35:13 -0300

    ✅ Update: Adding metatags

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
+      <Head>
+        <title>Página Principal</title>
+        <meta name='keywords' content='home dashboard get started' />
+        <meta name='description' content='página de exemplo para Next.JS' />
+      </Head>
       <div>
         <h1 className={styles.title}>Hello World, Next.JS!</h1>
       </div>
+    </>
   )
 }
