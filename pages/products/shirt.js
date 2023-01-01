23236a24ab3d06eea135f7478b7223a86bad5e40
David Almeida <contato.davidalmeida@outlook.com>
2023-01-01 20:00:55 -0300

    ✅ Update: Remove Navbar component

--- pages/products/shirt.js
+++ pages/products/shirt.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navbar from "../../components/Navbar";
 
 export default function Shirt() {
   return(
     <>
-      <Navbar />
       <h1>Camisa - R$19.19</h1>
       <Link href="/">Voltar</Link>
     </>
