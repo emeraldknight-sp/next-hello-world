f204f55d3f20affd20c4eadd8ab6316190dd7df5
David Almeida <contato.davidalmeida@outlook.com>
2023-01-02 10:22:12 -0300

    ✅ Update: Adding data with fetch API

--- pages/todos/[todoId].js
+++ pages/todos/[todoId].js
@@ -1,19 +1,45 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-export default function Todo() {
+export async function getStaticProps(context) {
+  const {params} = context
 
-  const router = useRouter();
+  const data = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${params.todoId}`
+  )
 
-  const todoId = router.query.todoId
+  const todo = await data.json()
+
+  return {
+    props: {
+      todo
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+  const data = response.json()
+
+  const paths = data.map((todo) => {
+    return {
+      params: {
+        todoId: `${todo.id}`
+      }
+    }
+  })
+
+  return { paths, fallback: false }
+}
+
+export default function Todo({ todo }) {
 
   return(
     <>
       <Link href="/todos">Voltar</Link>
-      <h1>Exibibindo o todo {todoId}</h1>
-      <p>Comentários: la la la... <Link href={`/todos/${todoId}/comments/1`}>Ler mais</Link></p>
-      <p>Comentários: le le le... <Link href={`/todos/${todoId}/comments/2`}>Ler mais</Link></p>
-      <p>Comentários: li li li... <Link href={`/todos/${todoId}/comments/3`}>Ler mais</Link></p>
+      <h1>Exibibindo o todo {todo.id}</h1>
+      <p>Comentários: la la la... <Link href={`/todos/${todo.id}/comments/1`}>Ler mais</Link></p>
+      <p>Comentários: le le le... <Link href={`/todos/${todo.id}/comments/2`}>Ler mais</Link></p>
+      <p>Comentários: li li li... <Link href={`/todos/${todo.id}/comments/3`}>Ler mais</Link></p>
     </>
   )
 }
